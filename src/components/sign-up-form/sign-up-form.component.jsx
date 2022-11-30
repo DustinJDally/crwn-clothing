99079b7e5f0d633186c206b51e3fae91596f55bc
Dustin Dally <dj.dally@protonmail.com>
2022-11-29 18:13:49 -0700

    Finished Section 7
    Added Context and Sign Out Function

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 
 import Button from "../button/button.component";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
 
 import './sign-up-form.styles.scss';
 
@@ -20,8 +22,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-
-  console.log(formFields);
+  const { setCurrentUser } = useContext(UserContext);
+  console.log('hit');
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -36,6 +38,8 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
+      setCurrentUser(user);
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
